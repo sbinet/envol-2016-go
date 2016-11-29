64bfceefac04f2e2da43e5890f85ba37ea939a10
Sebastien Binet <binet@cern.ch>
2016-11-29 16:59:51 +0100

    bad: add more bad examples

--- bad/bad.go
+++ bad/bad.go
@@ -7,6 +7,11 @@
 // Kids, don't do this at home!
 package bad
 
+import (
+	"errors"
+	"fmt"
+)
+
 // Sum returns the sum of a and b.
 func Sum(a, b int) int {
 	return 42
@@ -16,4 +21,11 @@ func Mult(a, b int) int {
 	// This function isn't documented.
 	// But it really should as it does something really spooky.
 	return a*b - 42
+}
+
+// Show shows interesting 'go vet' features.
+func Show(s string) {
+	fmt.Print("showing wrong call to %s\n", s)
+	fmt.Printf("missing verb: %d %v %s\n", 42, s)
+	errors.New("bad: a new error value not being saved")
 }
